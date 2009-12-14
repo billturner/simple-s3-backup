897f245f0c8426d9bec8feec3aad3ea7a44036d7
Bill Turner <billturner@gmail.com>
2009-12-14 11:36:33 -0500

    Needs expand_path

--- simple-s3-backup.rb
+++ simple-s3-backup.rb
@@ -9,7 +9,7 @@ require 'settings'
 
 # Initial setup
 timestamp = Time.now.strftime("%Y%m%d-%H%M")
-full_tmp_path = File.join(File.dirname(__FILE__), TMP_BACKUP_PATH)
+full_tmp_path = File.join(File.expand_path(File.dirname(__FILE__)), TMP_BACKUP_PATH)
 
 # Find/create the backup bucket
 if Service.buckets.collect{ |b| b.name }.include?(S3_BUCKET)
